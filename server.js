b6f6449d07b0a19a5dcfe2e828bfb5c00d8bc100
stevensotelo <stevenbetancurt@hotmail.com>
2022-03-31 19:12:41 -0500

    fixed port

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const operacion = require("./route_operacion");
 
 app.use("/",operacion);
 
-app.listen(3002, err => {
+const port = process.env.PORT || 3002;
+app.listen(port, err => {
     if (err) return console.log(err);
     console.log(`Servidor corriendo en el puerto: 3002`);
 })
